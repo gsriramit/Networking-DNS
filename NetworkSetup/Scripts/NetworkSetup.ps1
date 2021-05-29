a380a5c581e39820179ef83d6b82646bfabb576e
Sriram.G <gsriramit@gmail.com>
2021-05-29 17:04:20 +0530

    parameter name changes

--- NetworkSetup/Scripts/NetworkSetup.ps1
+++ NetworkSetup/Scripts/NetworkSetup.ps1
@@ -8,15 +8,22 @@ $subscriptionId= ''
 #Set the exact tenant and subscription context if you have access to multiple tenants and subscriptions
 Set-AzContext -Tenant $tenantId -Subscription $subscriptionId
 
-#Deploy the Hub Vnet
+#Deploy the Hub & Spoke VNets
+#Note: The template will
+#    create the networks, 
+#   create VNET peering
+#   create the private DNS zones for each and setup for auto-registration 
 $resourceGroupName = 'Az360-SharedSvcs-rg'
-$hubNetworkTemplate = '.\Hub-template.json'
-$hubNetworkParamters = '.\Hub-parameters.json'
+$hubNetworkTemplate = '.\'
+$hubNetworkParamters = '.\Hub&SpokeNetworks-Parameters.json'
 
-New-AzResourceGroupDeployment -Name HubNetworkDeployment -ResourceGroupName $resourceGroupName `
+
+New-AzResourceGroupDeployment -Name HubNSpokeNetworkDeployment -ResourceGroupName $resourceGroupName `
 -WhatIf `
   -TemplateFile $hubNetworkTemplate `
   -TemplateParameterFile $hubNetworkParamters
+
+
 
 
 
